51f5fbdd18bf8854245746462a1f3c0282a92ca4
Tristan King <tristanking@sheridan.edu>
2026-09-04 04:36:04 +0000

    initial upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip> // For std::fixed and std::setprecision
+
+using namespace std; 
+
+// Function prototypes
+void sortScores(double* scores, int numScores);
+double calculateAverage(double* scores, int numScores);
+void awardMedals(double* scores, int numScores);
+void displayResults(double* scores, int numScores, double average);
+
+int main() {
+    // Get number of scores from user
+
+    // Dynamically allocate array for scores
+
+    // Get scores from user
+
+    // Sort scores
+
+    // Calculate average score
+
+    // Award medals
+
+    // Display results
+
+    // Deallocate memory for scores array
+
+    return 0;
+}
